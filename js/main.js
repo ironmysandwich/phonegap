6ed28df34a8d34c1e88dc6138a11ccaca2fa9740
dcrosland <devin.crosland@gmail.com>
2013-12-16 15:00:18 -0800

    typo fixes

--- js/main.js
+++ js/main.js
@@ -11,20 +11,19 @@ var app = {
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
     findByName: function() {
-        var self = this;
+		var self = this;
 		this.store.findByName($('.search-key').val(), function(employees) {
-			$('employee-list').html(self.employeeLiTpl(employees));
+			$('.employee-list').html(self.employeeLiTpl(employees));
 		});
-    },
+	},
     initialize: function() {
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 		var self = this;
 		this.store = new MemoryStore(function() {
-			self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
 		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($(#employee-li-tpl").html());
-    }
+	}
 	
 
 };
