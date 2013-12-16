751384d1e8ec953c2f8f86b163bf202681af9ef8
dcrosland <devin.crosland@gmail.com>
2013-12-16 14:54:47 -0800

    handlebar templates

--- js/main.js
+++ js/main.js
@@ -6,17 +6,15 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+        var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('employee-list').html(self.employeeLiTpl(employees));
+		});
     },
     initialize: function() {
 		var self = this;
@@ -24,6 +22,8 @@ var app = {
 			self.showAlert('Store Initialized', 'Info');
 		});
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($(#employee-li-tpl").html());
     }
 	
 
